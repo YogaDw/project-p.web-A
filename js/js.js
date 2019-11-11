6185ca467ee2f31f99f50f16a315a403e0091503
Surya prayoga <43308174+YogaDw@users.noreply.github.com>
2019-11-11 23:26:40 +0700

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,21 @@
+<script>
+function validasiFile(){
+    var inputFile = document.getElementById('file');
+    var pathFile = inputFile.value;
+    var ekstensiOk = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+    if(!ekstensiOk.exec(pathFile)){
+        alert('Silakan upload file yang memiliki ekstensi .jpeg/.jpg/.png/.gif');
+        inputFile.value = '';
+        return false;
+    }else{
+        //Pratinjau gambar
+        if (inputFile.files && inputFile.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById('pratinjauGambar').innerHTML = '<img src="'+e.target.result+'"/>';
+            };
+            reader.readAsDataURL(inputFile.files[0]);
+        }
+    }
+}
+</script>
