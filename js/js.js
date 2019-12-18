1d6630b198ffc192d70a31f0cd46ba2d42776faa
Surya prayoga <43308174+YogaDw@users.noreply.github.com>
2019-12-18 11:40:41 +0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,21 +1,23 @@
-<script>
-function validasiFile(){
-    var inputFile = document.getElementById('file');
-    var pathFile = inputFile.value;
-    var ekstensiOk = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-    if(!ekstensiOk.exec(pathFile)){
-        alert('Silakan upload file yang memiliki ekstensi .jpeg/.jpg/.png/.gif');
-        inputFile.value = '';
-        return false;
-    }else{
-        //Pratinjau gambar
-        if (inputFile.files && inputFile.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                document.getElementById('pratinjauGambar').innerHTML = '<img src="'+e.target.result+'"/>';
-            };
-            reader.readAsDataURL(inputFile.files[0]);
+function Login(){
+       
+    var username=document.login.username.value;
+    username=username.toLowerCase();
+    var password=document.login.password.value;
+    password=password.toLowerCase();
+   
+        if (username=="yoga" && password=="123"){
+        alert("Selamat anda berhasil login");
         }
-    }
-}
-</script>
+        else{
+        alert("User name dan password anda salah!");
+        }
+    }     
+    
+    
+    $(document).ready(function() {
+        $(".pesan").hide();
+        $(".baca_selengkapnya").click(function(){
+          $(".pesan").slideDown('slow');
+          $(this).hide();
+        })
+      });
